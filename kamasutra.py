3afac65de165182a2d86fac45c700b1d833fb53d
luisrizzo <43777252+luisrizzo@users.noreply.github.com>
2018-10-24 22:30:55 +0200

    Update kamasutra.py

--- kamasutra.py
+++ kamasutra.py
@@ -3,21 +3,33 @@ from PyQt4.QtGui import *
 
 app = QApplication(sys.argv)
 
-window = QWidget()
-window.setWindowTitle("Kamasutra")
-window.show()
-window.resize(8 * 64 , 8 * 64)
-grid = []
-layout = QGridLayout(window)
-label = QLabel()
-label.setPixmap(QPixmap("images/transporter.png"))
-label2 = QLabel()
-label2.setPixmap(QPixmap("images/aether.png"))
-layout.addWidget (label,0,0)
-layout.addWidget (label2,7,7)
-layout.addWidget (label2,5,5)
-#for x in range(8):
-#    for y in range(8):
-#        layout.addWidget (label,y,x)
+class Example(QWidget):
+    
+    def __init__(self):
+        super(Example, self).__init__()
+        
+        self.initUI()
+        
+    def initUI(self):      
 
+        self.btn = QPushButton('Dialog', self)
+        self.btn.move(20, 20)
+        self.btn.clicked.connect(self.showDialog)
+        
+        self.le = QLineEdit(self)
+        self.le.move(130, 22)
+        
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('Input dialog')
+        self.show()
+        
+    def showDialog(self):
+        
+        text, ok = QInputDialog.getText(self, 'Input Dialog', 
+            'Enter your name:')
+        
+        if ok:
+            self.le.setText(str(text))
+
+ex = Example()
 sys.exit(app.exec_())
